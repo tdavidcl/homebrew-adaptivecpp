9701dd7e64717291c7457a59f49afa89075bbe91
David--Cléris Timothée <timothee.davidcleris@proton.me>
2025-05-12 10:55:20 +0200

    update to 25.02

--- Formula/adaptivecpp.rb
+++ Formula/adaptivecpp.rb
@@ -1,37 +1,44 @@
 class Adaptivecpp < Formula
   desc "SYCL and C++ standard parallelism for CPUs and GPUs"
-  homepage "https://github.com/AdaptiveCpp/AdaptiveCpp"
-  url "https://github.com/AdaptiveCpp/AdaptiveCpp/archive/refs/tags/v24.10.0.tar.gz"
-  sha256 "3bcd94eee41adea3ccc58390498ec9fd30e1548af5330a319be8ce3e034a6a0b"
+  homepage "https://adaptivecpp.github.io/"
+  url "https://github.com/AdaptiveCpp/AdaptiveCpp/archive/refs/tags/v25.02.0.tar.gz"
+  sha256 "8cc8a3be7bb38f88d7fd51597e0ec924b124d4233f64da62a31b9945b55612ca"
   license "BSD-2-Clause"
+  head "https://github.com/AdaptiveCpp/AdaptiveCpp.git", branch: "develop"
 
   depends_on "cmake" => :build
   depends_on "boost"
-  depends_on "libomp"
   depends_on "llvm"
-  depends_on "python"
+  uses_from_macos "python"
+
+  on_macos do
+    depends_on "libomp"
+  end
 
   def install
-    libomp_root = Formula["libomp"].opt_prefix
+    args = []
+    args << "-DLLVM_DIR=#{Formula["llvm"].opt_lib}/cmake/llvm" if OS.linux?
+    if OS.mac?
+      libomp_root = Formula["libomp"].opt_prefix
+      args << "-DOpenMP_ROOT=#{libomp_root}"
+    end
 
-    puts "libomp root: #{libomp_root}"
-
-    system "cmake", ".", *std_cmake_args, "-DOpenMP_ROOT=#{libomp_root}"
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
 
     # Avoid references to Homebrew shims directory
-    shim_references = [prefix/"etc/AdaptiveCpp/acpp-core.json"]
-    inreplace shim_references, Superenv.shims_path/ENV.cxx, ENV.cxx
+    inreplace prefix/"etc/AdaptiveCpp/acpp-core.json", Superenv.shims_path/ENV.cxx, ENV.cxx
+    return unless OS.mac?
 
     # we add -I#{libomp_root}/include to default-omp-cxx-flags
     inreplace prefix/"etc/AdaptiveCpp/acpp-core.json",
-      "\"default-omp-cxx-flags\" : \"", "\"default-omp-cxx-flags\" : \"-I#{libomp_root}/include "
-
-    system "cat", prefix/"etc/AdaptiveCpp/acpp-core.json"
+              "\"default-omp-cxx-flags\" : \"",
+              "\"default-omp-cxx-flags\" : \"-I#{libomp_root}/include "
   end
 
   test do
-    system "#{bin}/acpp", "--version"
+    system bin/"acpp", "--version"
 
     (testpath/"hellosycl.cpp").write <<~C
       #include <sycl/sycl.hpp>
